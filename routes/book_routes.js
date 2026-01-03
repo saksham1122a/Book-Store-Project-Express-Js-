a2843af1c54c08788b21077d8cf0233d9a959958
saksham1122a <sakshamnnda01@gmail.com>
2026-01-03 18:01:42 +0530

    MVC Architecture followed!

--- routes/book_routes.js
+++ routes/book_routes.js
@@ -1,56 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { BOOKS } = require('../models/book');
+const controller = require('../controller/book.controller')
 
-router.get('/', (req, res) => {
-    res.json(BOOKS);  // Convert to JSON and send response
-});
-
+router.get('/', controller.getAllBooks); // Route to get all books
 // Now the concept when i want to get a particular book by id, I can make the route dynamic by using route parameters
 // Here ':id' is a route parameter which make the route parameter dynamic.
-router.get('/:id', (req, res) => {
-    const id = parseInt(req.params.id); // Get the id from the URL
-    if(isNaN(id)){
-        return res.status(400).json({error: 'id must be type number'}); // If id is not a number, send 400
-    }
-    const book = BOOKS.find((e) =>e.id == id); // Find the book with the given id
-    if(!book){
-        return res.status(404).json({error: `Book with id ${id} does not exist`}); // If book not found, send 404
-    }
-    return res.json(book); // Send the book as response
-});
-
-router.post('/', (req, res) => {
-    // console.log(req.headers);
-    // console.log(req.body); // Log the request body
-    const { title, author} = req.body; // Destructure title and author from request body
-
-    if(!title || title === '')
-        return res.status(400).json({error: 'Title is required'}); // If title is missing, send 400
-    
-     if(!author || author === '')
-        return res.status(400).json({error: 'Author is required'});
-
-     const id = BOOKS.length + 1; // Get the current length of books array
-
-     const book = { id, title, author}; // Create a new book object
-        BOOKS.push(book); // Add the new book to the books array
-
-    return res.status(201).json({message: `Book created success`, id}); // Send success response with 201 status code
-});
-
-router.delete('/:id', (req, res) => {
-     const id = parseInt(req.params.id); // Get the id from the URL
-    if(isNaN(id)){
-        return res.status(400).json({error: 'id must be type number'}); // If id is not a number, send 400
-    }
-    const indexToDelete = BOOKS.findIndex((e) => e.id === id);
-
-    if(indexToDelete < 0)
-        return res.status(404).json({error: `Book with id ${id} does not exists!`});
-    BOOKS.splice(indexToDelete, 1);
-    return res.status(200).json({message: `Book Deleted!`});
-
-})
+router.get('/:id', controller.getBookById);
+router.post('/', controller.createBook);
+router.delete('/:id', controller.deleteBookById);
 
 module.exports = router;
